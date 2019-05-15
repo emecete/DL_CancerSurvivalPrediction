18ff19c6fe8fa90b637c020313c78b9d8b349d09
Maria <mcttuf0@uma.es>
2019-05-15 17:56:45 +0200

    Test image

--- treeMap.R
+++ treeMap.R
@@ -7,10 +7,11 @@ setwd("~/INGENIERIA_DE_LA_SALUD/4CUARTO/SEGUNDO_CUATRIMESTRE/TFG/CODE/DL_CancerS
 pat1 <- read.csv("pat1.csv")
 #pat1$TCGA.19.1787.01 <- pat1$TCGA.19.1787.01-min(pat1$TCGA.19.1787.01)
 pat1$freq <- rep(1, nrow(pat1))
-pat1$colors <- rainbow(nlevels(pat1$TCGA.19.1787.01))[pat1$TCGA.19.1787.01]
+#pat1$colors <- rainbow(nlevels(pat1$TCGA.19.1787.01))[pat1$TCGA.19.1787.01]
 
 # treemap
-a =treemap(pat1[seq(1,10000),],
+png(filename="prueba.png")
+treemap(pat1[seq(1,10000),],
         index=c("Functional.Annotation.Subgroup","Functional.Annotation","geneName"),
         vSize="freq",
         #type="value",
@@ -24,6 +25,9 @@ a =treemap(pat1[seq(1,10000),],
         type="value",
         palette="RdYlBu"
 )
+
+dev.off()
+
 
 library(plyr)
 counts <- ddply(pat1, .(pat1$Functional.Annotation.Subgroup, pat1$Functional.Annotation), nrow)
